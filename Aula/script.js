1460a7096cf86297cbe361c26a13dba00207fc03
Aster <douglascontasimportantes@protonmail.com>
2023-10-26 23:38:43 -0300

    :tada: New Start

---
+++ Aula/script.js
@@ -0,0 +1,16 @@
+function toggleMode() {
+    const html = document.documentElement
+
+    // Switch light and dark
+    html.classList.toggle('light')
+    
+    // Switch avatar
+    const img = document.querySelector("#profile img")
+
+    if(html.classList.contains('light')) {
+        img.setAttribute('src', "../assets/avatar-light.png")        
+    } else {
+        img.setAttribute('src', "../assets/avatar.png")
+    }
+    
+}
